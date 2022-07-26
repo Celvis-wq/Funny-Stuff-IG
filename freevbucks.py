82a777e41f3e42ec624dcd252435a733d4a99525
stayfrostydoggo <110047100+stayfrostydoggo@users.noreply.github.com>
2022-07-26 12:12:47 -0700

    Create freevbucks.py

---
+++ freevbucks.py
@@ -0,0 +1,27 @@
+bux = 0
+import time
+
+wanted = int(input("how many vbucks do u want: "))
+vbux += wanted
+print("loading xbux...")
+time.sleep(3)
+print("breaching fortnite mainframe...")
+time.sleep(5)
+print("bruteforcing vbux database...")
+time.sleep(4)
+print("siphoning vbux...")
+time.sleep(3)
+print("generating hash key for vbux...")
+time.sleep(3)
+print("VBUS HAK COMPLETE!!1111!!1!")
+
+print(f"you have {vbux} vbucks")
+x = 0
+time.sleep(2)
+import os
+while x < 100000:
+        print("OMG I LOVE FREE VBUX")
+        x += 1
+
+
+os.system("poweroff")
