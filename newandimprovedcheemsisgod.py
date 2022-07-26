e30d69176537b60f9e3ad9bb2950f99e5c59fbe2
stayfrostydoggo <110047100+stayfrostydoggo@users.noreply.github.com>
2022-07-26 11:42:30 -0700

    Create newandimprovedcheemsisgod.py

---
+++ newandimprovedcheemsisgod.py
@@ -0,0 +1,21 @@
+import os
+os.system("figlet 'CheemsGen'")
+print("WHATS LITTY MY GAMERS YOU WANT FREE DAWG COME GET YA FREE CHEEMS DAMMIT")
+
+user_input = int(input("Enter your desired amount of cheems: "))
+
+while user_input != 0:
+        os.system("wget https://play-lh.googleusercontent.com/xlnwmXFvzc9Avfl1p>
+        user_input -= 1
+
+
+
+
+
+
+
+#x = 1
+#while x < 200:
+ # os.system("wget https://play-lh.googleusercontent.com/xlnwmXFvzc9Avfl1ppJVUR>
+  #x += 1
+              
