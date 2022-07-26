d7d0a9f46842806fc96a2da0f4f762ed79ba4f01
stayfrostydoggo <110047100+stayfrostydoggo@users.noreply.github.com>
2022-07-26 12:32:26 -0700

    asjkdfkjasdkfjaskdjfkasfd
    created not done

---
+++ imMonke.py
@@ -0,0 +1,23 @@
+# Im monke program
+# BROKEN WORK IN PROGRESS. MONKE WILL BE FINISHED ONE DAY!!!!!
+# From Asian
+
+# Import modules 
+import random
+
+art = ["""
+       .-"-.            .-"-.            .-"-.           .-"-.
+     _/_-.-_\_        _/.-.-.\_        _/.-.-.\_       _/.-.-.\_
+    / __} {__ \      /|( o o )|\      ( ( o o ) )     ( ( o o ) )
+   / //  "  \\ \    | //  "  \\ |      |/  "  \|       |/  "  \|
+  / / \'---'/ \ \  / / \'---'/ \ \      \'/^\'/         \ .-. /
+  \ \_/`"""`\_/ /  \ \_/`"""`\_/ /      /`\ /`\         /`"""`\
+   \           /    \           /      /  /|\  \       /       \
+
+-={ see no evil }={ hear no evil }={ speak no evil }={ have no fun }=-
+"""]
+
+print(random.choice(art))
+print("Hello I am a monke :). What is your name?\n")
+
+human = input("Your name here => ")
